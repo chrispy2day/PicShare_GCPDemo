7980969cb4e791651fa4078e66c9426f26179b57
Chris Plowman <chris@jhaichrispy.com>
2017-10-20 17:49:57 -0700

    Add a DbTest page and cleans up some code

--- PicShare_GCPDemo/Controllers/HomeController.cs
+++ PicShare_GCPDemo/Controllers/HomeController.cs
@@ -5,28 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PicShare_GCPDemo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace PicShare_GCPDemo.Controllers
 {
     public class HomeController : Controller
     {
+        PicShareContext _context;
+
+        public HomeController(PicShareContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult DbTest()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var model = _context.Picture.OrderBy(p => p.AddedDate).Take(5).ToList();
+            return View(model);
         }
 
         public IActionResult Error()
